751c0949382de357929a02b01d2057aa9337c709
n1kson1 <110242007+n1kson1@users.noreply.github.com>
2022-08-31 17:14:26 +0300

    Update Program.cs

--- trapecia/Program.cs
+++ trapecia/Program.cs
@@ -1,55 +1,62 @@
-﻿class Trapezoid
+class Programm
 {
-
-    public static float TrapezoidSquare(float a, float b, float h)
-    {
-        return ((a + b) * h) / 2;
-    }
-
-    public static float TrapezoidPerimetr(float AB, float BC, float CD, float AD)
-    {
-        return (AB + BC + CD + AD);
-    }
-
     public static void Main()
     {
-        float a = 9;
-        float b = 6;
-        float c = 16;
-        float d = 4;
-        float h = 14;
+        Trapezoid figure = new Trapezoid(9, 6, 16, 4, 14);
 
-        float AB = a + b;
-        float BC = b + c;
-        float CD = c + d;
-        float AD = a + d;
-
-        Console.Write("Площадь трапеции : ");
-        Console.WriteLine(TrapezoidSquare(a, b, h));
-
-        Console.Write("Периметр трапеции : ");
-        Console.WriteLine(TrapezoidPerimetr(AB,BC,CD,AD));
-
-        Console.Write("Сторона АВ = ");
-        Console.WriteLine($"{AB}");
-
-        Console.Write("Сторона BC = ");
-        Console.WriteLine($"{BC}");
-
-        Console.Write("Сторона CD = ");
-        Console.WriteLine($"{CD}");
-
-        Console.Write("Сторона AD = ");
-        Console.WriteLine($"{AD}");
-
-        if (AB == CD)
+        float TrapezoidSquare(float a, float b, float h)
         {
-            Console.WriteLine("Это равнобокая трапеция");
+            return ((a + b) * h) / 2;
         }
-        else
+
+        float TrapezoidPerimetr(float AB, float BC, float CD, float AD)
         {
-            Console.WriteLine("Это не равнобокая трапеция");
+            return (AB + BC + CD + AD);
         }
     }
 
+    class Trapezoid
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int d;
+        private int h;
+
+        public Trapezoid(int a, int b, int c, int d, int h)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.h = h;
+
+            
+            float AB = a + b;
+            float BC = b + c;
+            float CD = c + d;
+            float AD = a + d;
+            float TrapezoidPerimetr = AB + BC + CD + AD;
+            float TrapezoidSquare = ((a + b) * h) / 2;
+
+
+            Console.Write("Площадь трапеции  : ");
+            Console.WriteLine($"{TrapezoidSquare}");
+
+            Console.Write("Периметр трапеции : ");
+            Console.WriteLine($"{TrapezoidPerimetr}");
+
+            Console.Write("Сторона АВ = ");
+            Console.WriteLine($"{AB}");
+
+            Console.Write("Сторона BC = ");
+            Console.WriteLine($"{BC}");
+
+            Console.Write("Сторона CD = ");
+            Console.WriteLine($"{CD}");
+
+            Console.Write("Сторона AD = ");
+            Console.WriteLine($"{AD}");
+        }
+    }
 }
